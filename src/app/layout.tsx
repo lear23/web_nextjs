c5cfe9bc6af6d64a2d3cfd9cd075e506392e5122
lear23 <130910243+lear23@users.noreply.github.com>
2025-07-25 11:38:22 +0200

    Add header component

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Header from "@/components/Header";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   <ClerkProvider>
+    <html lang="en">
+        <body >
+          <Header />
+          {children}
+      </body>
+    </html>
+   </ClerkProvider>
+  );
+}
